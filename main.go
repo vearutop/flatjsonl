fcf6a5b147e2802e8e0e1dbad2242cc83b23ce71
Viacheslav Poturaev <vearutop@gmail.com>
2022-06-29 01:19:26 +0200

    Add build task

--- main.go
+++ main.go
@@ -3,17 +3,28 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 
+	"github.com/bool64/dev/version"
 	"github.com/vearutop/flatjsonl/flatjsonl"
 )
 
 func main() {
+	var showVersion bool
+
 	f := flatjsonl.Flags{}
 
 	f.Register()
+	flag.BoolVar(&showVersion, "version", false, "Show version and exit.")
 	f.Parse()
+
+	if showVersion {
+		fmt.Println(version.Info().Version)
+
+		return
+	}
 
 	inputs := f.Inputs()
 	if len(inputs) == 0 {
