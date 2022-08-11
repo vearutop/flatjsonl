226e5ac1260307f730e427004e63e89451dd52f2
Viacheslav Poturaev <vearutop@gmail.com>
2022-08-11 16:56:13 +0200

    Improve to_snake_case (#14)

--- flatjsonl/keys_test.go
+++ flatjsonl/keys_test.go
@@ -13,4 +13,5 @@ func Test_toSnakeCase(t *testing.T) {
 	assert.Equal(t, "foo_bar", toSnakeCase("Foo_Bar"))
 	assert.Equal(t, "foo_bar", toSnakeCase("_Foo_Bar"))
 	assert.Equal(t, "foo_bar", toSnakeCase("__Foo-Bar"))
+	assert.Equal(t, "foo_bar varchar(500)", toSnakeCase("__Foo-Bar VARCHAR(500)"))
 }
